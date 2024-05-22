9c2a4160163e62eaacf20583baea62dc63366fca
Dohyun Yim <dohyunyim99@gmail.com>
2024-05-22 11:03:58 +0900

    dh05221103

--- src/components/man/cus/InfoCus.js
+++ src/components/man/cus/InfoCus.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import InfoLine from "./InfoLine";
 import styles from "./InfoCus.module.css";
 
 import CusCall from "../../../assets/man/CusCall_navy.png";
 import CusEmail from "../../../assets/man/CusEmail_navy.png";
+import axiosInstance from "../../../lib/axios";
 
-const InfoCus = ({ name, customerId, email, phone }) => {
+const InfoCus = () => {
+  const { id } = useParams();
+  const [userInfo, setUserInfo] = useState({});
+
+  const fetchUserInfo = async () => {
+    try {
+      const response = await axiosInstance.get(`api/v1/user/${id}`);
+      setUserInfo(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching user data: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserInfo();
+  }, [id]);
+
   return (
     <div className={styles.cusInfo}>
       <div className={styles.cusInfoTop}>
-        <div className={styles.cusInfoName}>{name} 고객님</div>
-        <div className={styles.cusInfoId}>{customerId}</div>
+        <div className={styles.cusInfoName}>{userInfo.name} 고객님</div>
+        <div className={styles.cusInfoId}>{userInfo.id}</div>
       </div>
-      <hr className={styles.separator} /> {/*top과 bottom 사이의 선 */}
+      <hr className={styles.separator} /> {/* top과 bottom 사이의 선 */}
       <div className={styles.cusInfoBottom}>
-        <InfoLine imgSrc={CusCall} contents={phone} />
-        <InfoLine imgSrc={CusEmail} contents={email} />
+        <InfoLine imgSrc={CusCall} contents={userInfo.phone} />
+        <InfoLine imgSrc={CusEmail} contents={userInfo.email} />
       </div>
     </div>
   );
--- src/components/man/cus/TableCus.js
+++ src/components/man/cus/TableCus.js
@@ -1,35 +1,49 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import InfoCus from "./InfoCus";
 import styles from "./TableCus.module.css";
 
 const TableCus = ({ customers }) => {
   return (
-    <table className={styles.CusTable}>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>이름</th>
-          <th>이메일</th>
-          <th>전화번호</th>
-          <th>더보기</th>
-        </tr>
-      </thead>
-      <tbody>
-        {customers.map((customer) => (
-          <tr key={customer.id}>
-            <td>{customer.id}</td>
-            <td>{customer.name}</td>
-            <td>{customer.email}</td>
-            <td>{customer.phone}</td>
-            <td>
-              <Link to={`${customer.id}`} className={styles.linkMore}>
-                ＞
-              </Link>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <Router>
+      <Routes>
+        <Route
+          path="/customers"
+          element={
+            <table className={styles.CusTable}>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>이름</th>
+                  <th>이메일</th>
+                  <th>전화번호</th>
+                  <th>더보기</th>
+                </tr>
+              </thead>
+              <tbody>
+                {customers.map((customer) => (
+                  <tr key={customer.id}>
+                    <td>{customer.id}</td>
+                    <td>{customer.name}</td>
+                    <td>{customer.email}</td>
+                    <td>{customer.phone}</td>
+                    <td>
+                      <Link
+                        to={`/customers/${customer.id}`}
+                        className={styles.linkMore}
+                      >
+                        ＞
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          }
+        />
+        <Route path="/customers/:id" element={<InfoCus />} />
+      </Routes>
+    </Router>
   );
 };
 
